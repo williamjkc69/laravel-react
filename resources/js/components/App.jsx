02b8b6cf6b56f502367156803d163aaa470ee11d
Tedor Talov <teodor@talov.com>
2020-07-28 21:54:28 -0600

    fix(app.jsx): refactor app.jsx and add favicon

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-export default class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Hola, Este es el App.</div> 
+export default function App() {
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card text-center">
+                        <div className="card-header">
+                            <h2>React Component in Laravel</h2>
+                        </div>
+
+                        <div className="card-body">
+                            I'm tiny React component in Laravel app!
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
+if (document.getElementById("app")) {
+    ReactDOM.render(<App />, document.getElementById("app"));
 }
